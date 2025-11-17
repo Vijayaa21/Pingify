32eeb8662ae03a30bb0ae479b5ffaab77c227bb4
Vijaya Mishra <134155725+Vijayaa21@users.noreply.github.com>
2025-11-17 22:08:42 +0530

    fix: correct spelling of messageSchema in message model

--- server/src/models/message.model.js
+++ server/src/models/message.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const mesaageSchema = new mongoose.Schema(
+const messageSchema = new mongoose.Schema(
     {
         senderId:{
             type: mongoose.Schema.Types.ObjectId,
@@ -21,6 +21,6 @@ const mesaageSchema = new mongoose.Schema(
     }, { timestamps: true}
 );
 
-const Message = mongoose.model("Message", mesaageSchema);
+const Message = mongoose.model("Message", messageSchema);
 
 export default Message;
